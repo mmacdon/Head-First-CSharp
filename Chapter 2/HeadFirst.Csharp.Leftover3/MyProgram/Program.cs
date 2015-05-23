80ce44b532230b5b70ef3eba64577b9fb3a4ebaf
Mary Macdonald <mary.macd@gmail.com>
2015-05-23 18:02:14 +0800

    First commit

---
+++ Chapter 2/HeadFirst.Csharp.Leftover3/MyProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeadFirst.Csharp.Leftover3;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy guy = new Guy("Joe", 43, 125);
+            HiThereWriter.HiThere(guy.Name);
+        }
+    }
+}
